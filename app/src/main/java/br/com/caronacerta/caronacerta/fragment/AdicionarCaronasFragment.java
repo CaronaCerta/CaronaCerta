8b6af1127cd3fa321c22794493b3c8add0902fa5
alexcreto <live4fun>
2014-11-07 22:10:21 -0200

    Adicionar caronas

--- app/src/main/java/br/com/caronacerta/caronacerta/fragment/AdicionarCaronasFragment.java
+++ app/src/main/java/br/com/caronacerta/caronacerta/fragment/AdicionarCaronasFragment.java
@@ -2,11 +2,28 @@ package br.com.caronacerta.caronacerta.fragment;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 
 import br.com.caronacerta.caronacerta.R;
+import br.com.caronacerta.caronacerta.adapter.PlacesAutoCompleteAdapter;
 
 public class AdicionarCaronasFragment extends Fragment {
 
@@ -19,6 +36,77 @@ public class AdicionarCaronasFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_adicionar_caronas, container, false);
 
+        AutoCompleteTextView autoCompOriginView = (AutoCompleteTextView) rootView.findViewById(R.id.autocomplete_origin);
+        AutoCompleteTextView autoCompDestinView = (AutoCompleteTextView) rootView.findViewById(R.id.autocomplete_destination);
+        autoCompOriginView.setAdapter(new PlacesAutoCompleteAdapter(this.getActivity(), R.layout.autocomplete_item));
+        autoCompDestinView.setAdapter(new PlacesAutoCompleteAdapter(this.getActivity(), R.layout.autocomplete_item));
+
         return rootView;
+    }
+
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        String str = (String) adapterView.getItemAtPosition(position);
+        Toast.makeText(this.getActivity(), str, Toast.LENGTH_SHORT).show();
+    }
+
+    /* AUTOCOMPLETE API */
+    public static final String LOG_TAG = "ExampleApp";
+
+    public static final String PLACES_API_BASE = "https://maps.googleapis.com/maps/api/place";
+    public static final String TYPE_AUTOCOMPLETE = "/autocomplete";
+    public static final String OUT_JSON = "/json";
+
+    public static final String API_KEY = "AIzaSyC_n8kc6HNTM2yUlLm-ztqbTApWlueLhxo";
+
+    static public ArrayList<String> autocomplete(String input) {
+        ArrayList<String> resultList = null;
+
+        HttpURLConnection conn = null;
+        StringBuilder jsonResults = new StringBuilder();
+        try {
+            StringBuilder sb = new StringBuilder(PLACES_API_BASE + TYPE_AUTOCOMPLETE + OUT_JSON);
+            sb.append("?key=" + API_KEY);
+            sb.append("&components=country:br");
+            sb.append("&input=").append(URLEncoder.encode(input, "utf8"));
+
+            URL url = new URL(sb.toString());
+            conn = (HttpURLConnection) url.openConnection();
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+
+            // Load the results into a StringBuilder
+            int read;
+            char[] buff = new char[1024];
+            while ((read = in.read(buff)) != -1) {
+                jsonResults.append(buff, 0, read);
+            }
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Error processing Places API URL", e);
+            return resultList;
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error connecting to Places API", e);
+            return resultList;
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "ARGHHHHHHHHHHH!!!!!", e);
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+
+        try {
+            // Create a JSON object hierarchy from the results
+            JSONObject jsonObj = new JSONObject(jsonResults.toString());
+            JSONArray predsJsonArray = jsonObj.getJSONArray("predictions");
+
+            // Extract the Place descriptions from the results
+            resultList = new ArrayList<String>(predsJsonArray.length());
+            for (int i = 0; i < predsJsonArray.length(); i++) {
+                resultList.add(predsJsonArray.getJSONObject(i).getString("description"));
+            }
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Cannot process JSON results", e);
+        }
+
+        return resultList;
     }
 }
